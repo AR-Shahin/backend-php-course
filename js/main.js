5e0a8d4efc6ba97811ac8ac47457560fc21fdcaa
AR-Shahin <mdshahinmije96@gmail.com>
2025-01-24 22:08:31 +0600

    =js 1

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// console.log("Hello");
+
+// var varName = "Shahin";
+
+var pi = 3.1416
+
+
+let person = {
+    'name' : 'shahin',
+    'age' : 27,
+    'is_graduate' : true
+}
+
+// console.log(person.name)
+var varName = 3;
+{let another = "ashin";}
+// console.log(typeof varName)
+varName = "shahin";
+
+
+
+// console.log(typeof varName)
+const PI = 9.9;
+
+
+
+let a  = 10;
+let b = 20;
+
+
+// console.log(a+b)
+
+
+pi = 5;
+
+
+function sum(a,b){
+    return a + b;
+}
+
+
+const sub = (a,b) => {
+    return a - b;
+}
+
+const test = (a) => a + 10;
+
+
+// console.log(sub(200,80))
+
+
+
+let nums = [10,30,"ars",person];
+
+
+// for(let i = 0;i<nums.length;i++){
+//     console.log(nums[i])
+// }
+
+
+// for(let p in person){
+//     console.log(p)
+// }
+
+// switch(ch){
+//     case 3:
+//         console.log("00");
+//         break;
+//     default:
+//         console.log("000")
+// }
+
+
+// if(10 > 20){
+//     console.log("NO");
+// }else{
+//     console.log("Yes");
+// }
+
+
+class Test{
+    a(){
+        console.log("A")
+    }
+}
+
+let c = new Test();
+
+c.a();
+
