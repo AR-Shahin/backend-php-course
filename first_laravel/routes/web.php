ec2dc237ad5de5231c99539d5a351f4312c97575
AR-Shahin <mdshahinmije96@gmail.com>
2025-05-04 22:10:35 +0600

    Quick issue fix

--- first_laravel/routes/web.php
+++ first_laravel/routes/web.php
@@ -1,12 +1,75 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect()->route("profile");
     return view('welcome');
 });
 
 
-Route::get("/about",function(){
-    return "About";
+// Route::get("/about",function(){
+//     return view("about");
+// });
+
+
+
+Route::get("/about",[AboutController::class,"index"]);
+
+Route::get("profile",function(){
+    return "Profile";
+})->name("profile");
+
+// Route::post("profile",function(){
+//     return "Profile";
+// });
+
+// Route::put("profile",function(){
+//     return "Profile";
+// });
+
+// Route::delete("profile",function(){
+//     return "Profile";
+// });
+
+
+// Route::prefix("users/")->middleware()->controller()->name()->group(function(){
+//     Route::get("/all",function(){
+//         return "all user";
+//     });
+//     Route::get("store",function(){
+//         return "all user";
+//     });
+// });
+
+Route::get("/users/{id}/{name?}",function($id,$name = null,Request $request){
+
+    return $request->all();
+    return "user $id $name";
+});
+
+Route::get("/users/create",function(){
+    return "all user";
+});
+
+Route::get("/users/edit",function(){
+    return "all user";
+});
+
+Route::get("/users/delete",function(){
+    return "all user";
+});
+Route::get("/users/update",function(){
+    return "all user";
+});
+
+
+// Route::resource("products",)
+
+
+
+Route::fallback(function(){
+    return "Route not found!";
 });
